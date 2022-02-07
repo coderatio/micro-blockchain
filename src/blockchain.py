85a156de3b02ba43125eb59cc20a86488e014853
Yahaya Josiah <josiahoyahaya@gmail.com>
2022-02-07 20:14:38 +0100

    Updated blockchain.py to have dynamic data

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,8 +1,9 @@
 from src.core.block import Block
 from src.core.blockheader import BlockHeader
-from src.util.util import hash256
+from src.util.util import hash256, random_bits
 import time
 import json
+import sys
 
 
 ZERO_HASH = '0' * 64
@@ -23,16 +24,34 @@ class Blockchain:
         timestamp = int(time.time())
         transaction = f"{height} Bitcoins has been mined successfully."
         merkle_root = hash256(transaction.encode()).hex()
-        bits = 'ffff001f' # You can make this dynamic
-        header = BlockHeader(VERSION, previous_hash,merkle_root, timestamp, bits)
+        bits = random_bits()  # You can make this dynamic
+        header = BlockHeader(VERSION, previous_hash, merkle_root, timestamp, bits)
         header.mine()
-        self.chain.append(Block(height, 1, header.__dict__, 1, transaction).__dict__)
-        print(json.dumps(self.chain, indent=4)) # This is to print output on console. You can remove it.
+        trx_count = len(self.chain) + 1
+        block_size = self.__get_block_size(height, header, trx_count, transaction)
+        
+        self.chain.append(Block(height, hex(block_size), header.__dict__, trx_count, transaction).__dict__)
+        print(json.dumps(self.chain, indent=4))  # This is to print output on console. You can remove it.
+
+    @staticmethod
+    def __get_block_size(height, header, trx_count, transaction):
+        block_data = [
+            {
+                height: height,
+                header: header.__dict__,
+                trx_count: trx_count,
+                transaction: transaction
+            }
+        ]
+
+        return sys.getsizeof(block_data)
 
     def run(self):
         while True:
             last_block = self.chain[::-1]
-            height = last_block[0]['height'] + 1
+            last_block_height = last_block[0]['height']
+            new_height = last_block_height
+            height = hex(new_height + 1) if isinstance(last_block_height, int)  else hex(int(new_height, base=16) + 1)
             previous_hash = last_block[0]['header']['hash']
 
             self.add_block(height, previous_hash)
--- src/util/util.py
+++ src/util/util.py
@@ -1,6 +1,11 @@
 import hashlib
+import random
 
 
 def hash256(string):
     """Creates two round solve of hash 256"""
     return hashlib.sha256(hashlib.sha256(string).digest()).digest()
+
+def random_bits():
+    bits = [''.join([random.choice('ABCDEF0123456789') for i in range(6)])]
+    return bits[0]
